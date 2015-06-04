fc7d0e61ce0e5f7934289a45922c4d3d102c1259
Daniel Williams <mail@daniel-williams.co.uk>
2015-06-04 14:50:00 +0100

    Initial version of the code

---
+++ tests/test_pulsar_telescope.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_pulsar_telescope
+----------------------------------
+
+Tests for `pulsar_telescope` module.
+"""
+
+import unittest
+
+from pulsar_telescope import pulsar_telescope
+
+
+class TestPulsar_telescope(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
